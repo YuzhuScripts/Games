5a16f4083f0825644e160d705d34c44c16986885
Yuzhu <kuritoalt4@gmail.com>
2025-02-16 17:34:17 +0800

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 16732694052 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YuzhuScripts/Games/main/fisch.lua"))()
 elseif game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YuzhuScripts/Games/main/fisch.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YuzhuScripts/Games/main/bf.lua"))()
 else
 game:GetService("Players").LocalPlayer:Kick("\nGame is not supported!")
 end
