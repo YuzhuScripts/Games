416578e063c436c55a3cd6bf1af9b9220d20824b
Yuzhu <kuritoalt4@gmail.com>
2025-02-17 20:29:24 +0800

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,3 +1,10 @@
+[[-----------------]]
+repeat task.wait() until game.Players.LocalPlayer
+_G.yuzhu = _G.yuzhu or false
+if _G.yuzhu then return end
+_G.yuzhu = true
+[[-----------------]]
+
 if game.PlaceId == 16732694052 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YuzhuScripts/Games/main/fisch.lua"))()
 elseif game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then 
